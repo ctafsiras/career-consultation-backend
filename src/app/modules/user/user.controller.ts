fad682484a02d6a3228e62737d7e9b189ee77471
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-10-19 09:13:50 +0600

    updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,12 +2,12 @@ import catchAsync from "../../../shared/catchAsync";
 import { UserService } from "./user.service";
 
 const create = catchAsync(async (req, res) => {
-  const user = await UserService.create(req.body);
+  const token = await UserService.create(req.body);
   res.status(201).json({
     success: true,
     statusCode: 201,
     message: "User created successfully",
-    data: user,
+    token
   });
 });
 
